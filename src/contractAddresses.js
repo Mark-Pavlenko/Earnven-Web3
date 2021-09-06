bddf98047b594c9daf5be31418f2ddb25d988866
Shivam Sharma <shivam691999@gmail.com>
2021-09-07 02:54:40 +0530

    Completed Sushi v2 Integration

---
+++ src/contractAddresses.js
@@ -0,0 +1,10 @@
+var addresses ={
+    uniRouter : '0x7a250d5630b4cf539739df2c5dacb4c659f2488d',
+    uniFactory : '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f',
+    sushiRouter : '0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F',
+    sushiFactory : '0xC0AEe478e3658e2610c5F7A4A2E1777cE9e4f2Ac',
+    oneClickUniV2Contract : '0x367964E1A5E7f5f1b859071572cF549f70683685',
+    oneClickSushiV2Contract : '0xE9616467dfc714ED5214f2bED909387Ef7Cd80b4'
+}
+
+export default addresses
