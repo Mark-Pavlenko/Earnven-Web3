08eacbec776442fb6108d554e670624fe33464ca
Prabhakaran <77061534+Prabhakaran-CBD@users.noreply.github.com>
2021-09-23 23:26:37 +0530

    Liquidity Pool Details Section Added

--- src/components/tokenDetails/index.js
+++ src/components/tokenDetails/index.js
@@ -1,35 +1,34 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 import SearchTokens from '../searchTokens'
-import { useParams } from 'react-router-dom';
-import {Chart} from '../Chart/Chart'
-import { useNavigate } from 'react-router'
+import { useParams, useNavigate } from 'react-router-dom'
+import { Chart } from '../Chart/Chart'
+//import { useNavigate } from 'react-router'
 import TransactionPerToken from '../transactionHistory/transactionPerToken copy'
 
-
 export default function Index() {
+  var navigate = useNavigate()
+  const { address } = useParams()
+  const { tokenid } = useParams()
+  console.log(tokenid)
 
-    var navigate = useNavigate();
-    const {address} = useParams();
-    const { tokenid } = useParams();
-    console.log(tokenid)
+  const [Token, setToken] = useState('')
 
-    const [Token, setToken] = useState('')
+  function callbackFunction(childData) {
+    setToken(childData)
+    navigate(`/app/token/${childData}`)
+  }
 
-    function callbackFunction(childData){
-        setToken(childData)
-        navigate(`/app/token/${childData}`)
-    }
-
-    return (
-        <div style={{margin:'auto'}}>
-            
-            {/* <span style={{visibility:'hidden'}}>{Token}
+  return (
+    <div style={{ margin: 'auto' }}>
+      {/* <span style={{visibility:'hidden'}}>{Token}
             <SearchTokens parentCallback = {callbackFunction}/> </span> */}
-            {/* <br/><br/><br/><br/> */}
-           {/*  <center><hr width='80%'/></center> */}
-            {/* {address} */}
-            <Chart/> 
-            <center><TransactionPerToken address={address} tokenid={tokenid}/></center>
-        </div>
-    )
+      {/* <br/><br/><br/><br/> */}
+      {/*  <center><hr width='80%'/></center> */}
+      {/* {address} */}
+      <Chart />
+      <center>
+        <TransactionPerToken address={address} tokenid={tokenid} />
+      </center>
+    </div>
+  )
 }
