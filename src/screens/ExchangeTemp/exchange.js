42b47fc64cf9f6fb13094852ede822bd8d5530c4
Rohit Yadav <rohit12242@Rohits-MacBook-Air.local>
2021-10-01 19:49:10 +0530

    pushed missing file

---
+++ src/screens/ExchangeTemp/exchange.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { StyledHomePageContainer } from "./styles";
+
+class ExchangeTemp extends Component {
+
+    render() {
+        return (
+            <StyledHomePageContainer>
+                <div className="main-container">
+                <div className="swap-container">
+                    
+                </div>
+                </div>
+            </StyledHomePageContainer>
+
+        );
+    }
+}
+
+
+export default ExchangeTemp;
