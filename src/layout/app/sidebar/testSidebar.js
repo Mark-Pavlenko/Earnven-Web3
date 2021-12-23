1eee073072563a56196a122e66f143fd8fca0d39
marko.pavlenko <mark.pavlenko@megadevllc.com>
2021-12-23 14:45:33 +0200

    fix: fix recent tokens api key

---
+++ src/layout/app/sidebar/testSidebar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function TestSidebar() {
+  return (
+    <section>
+      <nav style={{ float: 'left', width: '25%', backgroundColor: 'green' }}>
+        <ul>
+          <li>First</li>
+          <li>First</li>
+          <li>First</li>
+        </ul>
+      </nav>
+    </section>
+  );
+}
