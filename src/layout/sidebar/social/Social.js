ee75eed5e7840413a36f5f253df014f77ef135e2
marko.pavlenko <mark.pavlenko@megadevllc.com>
2022-01-06 13:18:28 +0200

    style: made network icons grid

--- src/layout/sidebar/social/Social.js
+++ src/layout/sidebar/social/Social.js
@@ -1,47 +1,73 @@
 import React from 'react';
+import './test.css';
 import { Box, Collapse, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
-import { SocialNetorksMainLayout, NetworksSubColumn } from './styles';
+import { NetworksSubColumn, NetworksGridList } from './styles';
 import github from '../../../assets/icons/github_menu.svg';
 import fb_menu_icon from '../../../assets/icons/facebook_menu_logo.svg';
 import other_menu_logo from '../../../assets/icons/discord_menu_logo.svg';
 import discord_menu from '../../../assets/icons/logo_4_menu.svg';
 import twitter_menu_logo from '../../../assets/icons/twitter_menu_logo.svg';
 import telegram_menu_logo from '../../../assets/icons/telegram_menu_logo.svg';
-import { experimentalStyled as styled } from '@material-ui/core/styles';
 import { useSelector } from 'react-redux';
 
-const Social = () => {
+const SocialNetworksGrid = () => {
   const themeType = useSelector((state) => state.themeReducer.isLightTheme);
   return (
-    <SocialNetorksMainLayout>
-      <NetworksSubColumn isLightTheme={themeType}>
-        <span>
-          <a className="link" href="url">
-            <img src={github} alt="" />
-          </a>
-        </span>
-        <a href="url">
-          <img src={discord_menu} alt="" />
-        </a>
-      </NetworksSubColumn>
-      <NetworksSubColumn isLightTheme={themeType}>
-        <a className="link" href="url">
-          <img src={other_menu_logo} alt="" />
-        </a>
-        <a className="link" href="url">
-          <img src={twitter_menu_logo} alt="" />
-        </a>
-      </NetworksSubColumn>
-      <NetworksSubColumn isLightTheme={themeType}>
-        <a className="link" href="url">
-          <img src={fb_menu_icon} alt="" />
-        </a>
-        <a className="link" href="url">
-          <img src={telegram_menu_logo} alt="" />
-        </a>
-      </NetworksSubColumn>
-    </SocialNetorksMainLayout>
+    <>
+      <NetworksGridList isLightTheme={themeType}>
+        <NetworksSubColumn>
+          <li>
+            <a href="url">
+              <img src={github} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="url">
+              <img src={discord_menu} alt="" />
+            </a>
+          </li>
+        </NetworksSubColumn>
+        <NetworksSubColumn>
+          <li>
+            <a className="link" href="url">
+              <img src={other_menu_logo} alt="" />
+            </a>
+          </li>
+          <li>
+            <a className="link" href="url">
+              <img src={twitter_menu_logo} alt="" />
+            </a>
+          </li>
+        </NetworksSubColumn>
+        <NetworksSubColumn>
+          <li>
+            <a className="link" href="url">
+              <img src={fb_menu_icon} alt="" />
+            </a>
+          </li>
+          <li>
+            <a className="link" href="url">
+              <img src={telegram_menu_logo} alt="" />
+            </a>
+          </li>
+        </NetworksSubColumn>
+      </NetworksGridList>
+    </>
+    // <SocialNetorksMainLayout>
+    //   <NetworksSubColumn isLightTheme={themeType}>
+    //     <div>
+
+    //     </div>
+
+    //   </NetworksSubColumn>
+    //   <NetworksSubColumn isLightTheme={themeType}>
+
+    //   </NetworksSubColumn>
+    //   <NetworksSubColumn isLightTheme={themeType}>
+
+    //   </NetworksSubColumn>
+    // </SocialNetorksMainLayout>
   );
 };
 
-export default Social;
+export default SocialNetworksGrid;
--- src/layout/sidebar/social/styles.js
+++ src/layout/sidebar/social/styles.js
@@ -1,33 +1,29 @@
 import styled from 'styled-components';
 
-export const SocialNetorksMainLayout = styled.div`
+export const NetworksGridList = styled.ul`
   display: inline-flex;
   flex-wrap: wrap;
   gap: 20px;
-
-  // width: 100px;
-  //background-color: red;
   margin-top: 30px;
   margin-left: 125px;
-`;
 
-export const NetworksSubColumn = styled.div`
-  overflow: hidden;
-
-  img:hover {
-    overflow: hidden;
+  li {
     padding: 4px;
+  }
+
+  li:first-child {
+    margin-bottom: 20px;
+  }
+
+  li:hover {
     border-radius: 10px;
     background-color: ${(props) => (props.isLightTheme ? '#ffffff' : '#1F265C')};
     box-shadow: ${(props) =>
       props.isLightTheme
         ? 'inset 0px 5px 10px -6px rgba(51, 78, 131, 0.12)'
         : 'inset 2px 2px 4px rgba(255, 255, 255, 0.1)'};
-
     backdrop-filter: ${(props) => (props.isLightTheme ? 'none' : 'blur(35px)')};
   }
-
-  img:first-child {
-    margin-bottom: 20px;
-  }
 `;
+
+export const NetworksSubColumn = styled.div``;
