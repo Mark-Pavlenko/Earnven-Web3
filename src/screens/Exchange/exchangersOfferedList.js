ccbe5435349da21fc142d2094ba7fc200ce51054
marko.pavlenko <mark.pavlenko@megadevllc.com>
2022-02-14 19:10:54 +0200

    feat: output mock exchangers list

---
+++ src/screens/Exchange/exchangersOfferedList.js
@@ -0,0 +1,119 @@
+import balancerExchangerIcon from '../../assets/icons/exchangers/balancerExchangerIcon.svg';
+import bancorExchangerIcon from '../../assets/icons/exchangers/bancorExchangerIcon.svg';
+import curveExchangerIcon from '../../assets/icons/exchangers/curveExchangerIcon.svg';
+import dodoExchangerIcon from '../../assets/icons/exchangers/dodoExchangerIcon.svg';
+import eth2DaiExchangerIcon from '../../assets/icons/exchangers/eth2DaiExchangerIcon.png';
+import kyberExchangerIcon from '../../assets/icons/exchangers/kyberExchangerIcon.svg';
+import multiBridgeExchangerIcon from '../../assets/icons/exchangers/miltiBridgeExchangerIcon.svg';
+import mooniswapExchangerIcon from '../../assets/icons/exchangers/mooniswapExchangerIcon.png';
+import mStableExchangerIcon from '../../assets/icons/exchangers/mStableExchangerIcon.svg';
+import shellExchangerIcon from '../../assets/icons/exchangers/shellExchangerIcon.svg';
+import sushiSwapExchangerIcon from '../../assets/icons/exchangers/sushiSwapExchangerIcon.svg';
+import swerveExchangerIcon from '../../assets/icons/exchangers/swerveExchangerIcon.png';
+import uniswapV2ExchangerIcon from '../../assets/icons/exchangers/uniswapV2ExchangerIcon.svg';
+import zeroExchangeIcon from '../../assets/icons/exchangers/0xExchangerIcon.svg';
+
+const exchangersOfferedList = [
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: true,
+    logoIcon: balancerExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: bancorExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: curveExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: dodoExchangerIcon,
+    greenDotIcon: true,
+  },
+
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: eth2DaiExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: kyberExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: multiBridgeExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: mooniswapExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: mStableExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: shellExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: sushiSwapExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: swerveExchangerIcon,
+    greenDotIcon: true,
+  },
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: uniswapV2ExchangerIcon,
+    greenDotIcon: true,
+  },
+
+  {
+    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
+    gasFee: '$10.03',
+    isBestRate: false,
+    logoIcon: zeroExchangeIcon,
+    greenDotIcon: true,
+  },
+];
+
+export default exchangersOfferedList;
--- src/screens/Exchange/multiSwap.js
+++ src/screens/Exchange/multiSwap.js
@@ -69,132 +69,16 @@ import PopupState, { bindPopover, bindTrigger } from 'material-ui-popup-state';
 import Popover from '@mui/material/Popover';
 import greenDot from '../../assets/icons/greenDot.svg';
 
-import balancerExchangerIcon from '../../assets/icons/exchangers/balancerExchangerIcon.svg';
-import bancorExchangerIcon from '../../assets/icons/exchangers/bancorExchangerIcon.svg';
-import curveExchangerIcon from '../../assets/icons/exchangers/curveExchangerIcon.svg';
-import dodoExchangerIcon from '../../assets/icons/exchangers/dodoExchangerIcon.svg';
-import eth2DaiExchangerIcon from '../../assets/icons/exchangers/eth2DaiExchangerIcon.png';
-import kyberExchangerIcon from '../../assets/icons/exchangers/kyberExchangerIcon.svg';
-// import liquidityProviderExchangerIcon from '../../assets/icons/exchangers/liquidityProviderExchangerIcon.svg';
-import multiBridgeExchangerIcon from '../../assets/icons/exchangers/miltiBridgeExchangerIcon.svg';
-import mooniswapExchangerIcon from '../../assets/icons/exchangers/mooniswapExchangerIcon.png';
-import mStableExchangerIcon from '../../assets/icons/exchangers/mStableExchangerIcon.svg';
 import paraSwapIcon from '../../assets/icons/exchangers/paraSwapExchangerIcon.svg';
-import shellExchangerIcon from '../../assets/icons/exchangers/shellExchangerIcon.svg';
-import sushiSwapExchangerIcon from '../../assets/icons/exchangers/sushiSwapExchangerIcon.svg';
-import swerveExchangerIcon from '../../assets/icons/exchangers/swerveExchangerIcon.png';
 import uniswapExchangerIcon from '../../assets/icons/exchangers/uniswapExchangerIcon.svg';
-import uniswapV2ExchangerIcon from '../../assets/icons/exchangers/uniswapV2ExchangerIcon.svg';
-import zeroExchangeIcon from '../../assets/icons/exchangers/0xExchangerIcon.svg';
+
+import exchangersOfferedList from './exchangersOfferedList';
 
 const useStyles = makeStyles((theme) => ({
   noBorder: {
     border: 'none !important',
   },
 }));
-
-const exchangersOfferedList = [
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: true,
-    logoIcon: balancerExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: bancorExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: curveExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: dodoExchangerIcon,
-    greenDotIcon: true,
-  },
-
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: eth2DaiExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: kyberExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: multiBridgeExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: mooniswapExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: mStableExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: shellExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: sushiSwapExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: swerveExchangerIcon,
-    greenDotIcon: true,
-  },
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: uniswapV2ExchangerIcon,
-    greenDotIcon: true,
-  },
-
-  {
-    receiveTokenUSDCurrencyCourse: '3510 DAI ($3510.03)',
-    gasFee: '$10.03',
-    isBestRate: false,
-    logoIcon: zeroExchangeIcon,
-    greenDotIcon: true,
-  },
-];
 
 export default function MultiSwapComponent() {
   const { address } = useParams();
