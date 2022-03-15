271bbc04d20cf17a8ce6f73e23465f5f185cf117
marko.pavlenko <mark.pavlenko@megadevllc.com>
2022-03-15 13:00:45 +0200

    feat: fix bug with NAN web3 errpr (getAmountMultiFunction)

--- src/api/api.js
+++ src/api/api.js
@@ -87,7 +87,7 @@ export const getTokenUSDAmount = async (tokenData) => {
     );
     finalUSDCurrencyValue = ethDollarValue.data.ethereum.usd * parseFloat(tokenData.amount);
   }
-  finalUSDCurrencyValue = finalUSDCurrencyValue.toFixed(3);
+  finalUSDCurrencyValue = finalUSDCurrencyValue.toFixed(2);
   return { ...tokenData.tokenData, USDCurrency: finalUSDCurrencyValue };
 };
 
